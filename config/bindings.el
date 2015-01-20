4285385cc5ef7088d0da3598a720ccbedce32602
Nicolas Ginder <nicolas-ginder@users.noreply.github.com>
2015-01-20 22:39:42 +0000

    add repos and bindings

--- config/bindings.el
+++ config/bindings.el
@@ -3,3 +3,6 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
