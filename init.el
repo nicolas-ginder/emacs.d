1da0bb9b2e238a07713f0ff58c6ec93fb718678d
Nicolas Ginder <nicolas-ginder@users.noreply.github.com>
2015-01-21 22:02:20 +0000

    setup helm and theme

--- init.el
+++ init.el
@@ -26,13 +26,15 @@
 
 
 (package-require 'helm)
+(package-require 'helm-swoop)
+(package-require 'cyberpunk-theme)
 
-(package-require 'color-theme-solarized)
-
+;;(package-require 'color-theme-solarized)
+(load-theme 'cyberpunk t)
 
 ;; helm
 ;(require 'helm)
-;(require 'helm-config)
+;(require 'hellm-config)
 
 ;(global-set-key (kbd "C-c h") 'helm-command-prefix)
 ;(global-unset-key (kbd "C-x c"))
@@ -52,3 +54,17 @@
 
 ;(helm-mode 1)
 ;(helm-autoresize-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;;'(custom-safe-themes (quote ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "fa942713c74b5ad27893e72ed8dccf791c9d39e5e7336e52d76e7125bfa51d4c" "293907f71094d7a1ecf5bcb366bf32c2af0df5f9f607ffb3cab14d1ae3a4262a" default)))
+ '(helm-mode t)
+ '(helm-autoresize-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
