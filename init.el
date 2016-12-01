7c5d67c859ebfbceb348dbea7c856e63581ea2d3
Nicolas Ginder <nicolas-ginder@users.noreply.github.com>
2016-12-01 20:28:11 +0000

    add recent files history

--- init.el
+++ init.el
@@ -51,3 +51,22 @@
 (global-set-key (kbd "M-I") 'helm-show-kill-ring)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+(require 'recentf)
+(recentf-mode 1)
+(run-at-time nil (* 30) 'recentf-save-list)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (haskell-mode restclient rainbow-delimiters pdf-tools js2-mode jq-mode helm-swoop dired+ cyberpunk-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
